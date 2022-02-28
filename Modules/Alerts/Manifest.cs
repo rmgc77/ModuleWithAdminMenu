3f666f8ba8f10e45bc4564abf3d87b0f9d9b9dea
Rui Camarinha <rui.camarinha@farfetch.com>
2022-02-28 17:32:45 +0000

    Add project files.

---
+++ Modules/Alerts/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "Alerts",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "Alerts",
+    Category = "Content Management"
+)]
